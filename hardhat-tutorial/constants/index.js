5e67f55934eb31fdc7a4cfc56271c14179ec468e
mwang <seeyouthere2@protonmail.com>
2023-06-12 10:36:53 +0800

    message

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xC1E5524214A8E9368eB706552264CA37A183d616";
+// URL for the Crypto Devs NFT Metadata
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
